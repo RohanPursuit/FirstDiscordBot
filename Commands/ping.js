d516ce3761378592cb8da974104c83c6ec42d1b5
Rohan-Sterling <rohansterling@pursuit.org>
2021-09-18 17:59:28 -0400

    add ping function

---
+++ Commands/ping.js
@@ -0,0 +1,8 @@
+export function ping(task){
+    if(!task.length) return "pong"
+    console.log(task)
+    return "Huh..."
+    // return `The ${msg.content.slice(1)} command is under construction`
+}
+
+
